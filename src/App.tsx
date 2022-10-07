8dbae35abac2072c487e2ccd0e64625f74b0ed87
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-07 10:50:15 +0330

    Create directory 'common' and 'ui' for component, made display and main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react'
+import Navbar from './components/common/Navbar'
 
 
 function App() {
   return (
     <div className='flex flex-col justify-between min-h-[calc(100vh)]'>
       <main className='flex items-center justify-center min-h-[calc(100vh - 60px)] w-[90%] md:w-[560px] mx-auto py-4 flex-grow'>
-        <div className='w-full'>
-        </div>
+        <Navbar />
       </main>
     </div>
   )
