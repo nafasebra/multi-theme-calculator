d5780af27cd46ed803523583bf26114ebbaed2bf
unknown <ebranafas@gmail.com>
2022-10-14 00:16:07 +0330

    Handle color variables css to tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        "dark": "#372413",
+        "bg-main": "var(--background-main)",
+        "bg-keypad": "var(--background-keypad)",
+        "bg-input": "var(--background-input)",
+        "bg-btn-primary": "var(--button-primary)",
+        "bg-btn-secondary": "var(--button-secondary)",
+        "bg-btn-third": "var(--button-third)",
+        "color-main": "var(--text-color)",
+        "color-btn-primary": "var(--text-button-primary)",
+        "color-btn-secondary": "var(--text-button-secondary)",
       },
     },
   },
